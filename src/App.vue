ad58409380aa444804421a335822a16100c306be
R1ce02 <rafi.kahn@shark-soft.com>
2021-07-06 19:05:06 +0200

    added imagebox and our mission section

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <navbar />
-  <div class="container-fluid no-padding">
-    <img class="img-fluid " src="./assets/code-pic.png"/>
+  <image-box />
+  <div class="container">
+    <h3>Our Mission</h3>
+    <h2>Offer State of The Art IT Solutions to Our Customers</h2>
+    <p>
+      Our mission is to offer our customers a customised and specialised IT
+      service. We customise our IT solutions to their needs and their
+      priorities. We use new and promising tools to create and develop our
+      products. <br> <br>Exciting challenges lie in our path and we are ready to take
+      them on! <br>Join us on our journey...
+    </p>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import imageBox from "./components/homePage_Image.vue";
 
 export default {
   components: {
     Navbar,
+    imageBox,
   },
 };
 </script>
@@ -21,11 +32,25 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   overflow-x: hidden;
+  font-size: 100%;
 }
 
-.no-padding {
-  padding-left: 0;
-  padding-right: 0;
-  margin: 0;
+h3 {
+  color: grey;
+  font-size: 1.5rem;
+  font-weight: 500;
+}
+
+h2 {
+  font-weight: 600;
+  font-size: 2rem;
+}
+
+p {
+  font-size: 2rem;
+}
+
+.container {
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/homePage_Image.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid no-padding">
+    <img class="img-fluid" src="../assets/code-pic.png" />
+    <h1>We Need Your Talent</h1>
+  </div>
+</template>
+
+<style scoped>
+.no-padding {
+  padding-left: 0;
+  padding-right: 0;
+  margin: 0;
+}
+
+.container-fluid {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+h1 {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-weight: 600;
+  font-size: 4rem;
+}
+
+img {
+  filter: brightness(50%);
+}
+
+@media screen and (max-width: 600px) {
+  h1 {
+    font-size: 30px;
+  }
+}
+</style>
