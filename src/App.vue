8e74f2bcef9b4c759b64ff748584a1f5d5430487
rafi khan <kha18876@spengergasse.at>
2021-07-06 16:55:56 +0200

    initialised project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <navbar />
+  <div class="container-fluid no-padding">
+    <img class="img-fluid " src="./assets/code-pic.png"/>
   </div>
-  <router-view/>
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  overflow-x: hidden;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.no-padding {
+  padding-left: 0;
+  padding-right: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-dark shadow-sm"
+    style="background: #1d5097"
+  >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="@/assets/shark_logo.png" height="35" />
+      </a>
+      <button type="button" class="btn btn-outline-light">Apply Now</button>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+</style>
