c7968a2569a549fcf33e28a937f9d0e27df32f59
R1ce02 <rafi.kahn@shark-soft.com>
2021-07-09 16:24:37 +0200

    corrected spelling mistakes

--- src/components/homePage_Offer.vue
+++ src/components/homePage_Offer.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="container">
     <h2 class="title">Apply For Your Future</h2>
-    <p class="txt">We are hiring people who have the thirst to become better. We will dedicate our time and effort trianing young talents. Students who are studying at a university can join us as a part-time employee. We open heartedly welcome interns too.</p>
-
+    <p class="txt">We are hiring people who have the thirst to become better. We will dedicate our time and effort training young talents. Students who are studying at a university can join us as a part-time employee. We open heartedly welcome interns too.</p>
   </div>
 </template>
 
