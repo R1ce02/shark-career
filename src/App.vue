049a5d80838daf0448d74a0c9bde037c71e51140
R1ce02 <rafi.kahn@shark-soft.com>
2021-07-12 12:07:19 +0200

    font changes

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
 
 <style>
 #app {
-  font-family: "Open Sans", sans-serif;
+  font-family: Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   overflow-x: hidden;
